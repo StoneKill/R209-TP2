ed2e40ebf86782f613fe52e84500bc639b1f9294
StoneKill <yanis290105@outlook.fr>
2024-05-06 15:26:29 +0200

    Shifumi qui fonctionne

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-let btnP = document.getElementById('P').addEventListener("click", PFC("P"))
-let btnF = document.getElementById('F').addEventListener("click", PFC("F"))
-let brnS = document.getElementById('C').addEventListener("click", PFC("C"))
+const btnP = document.getElementById('P').addEventListener('click', () => { PFC("pierre") })
+const btnF = document.getElementById('F').addEventListener('click', () => { PFC("feuille") })
+const btnC = document.getElementById('C').addEventListener('click', () => { PFC("ciseaux") })
 
-function PFC() {
-    
+function PFC(selectionJoueur) {
+    const choix = ['pierre', 'feuille', 'ciseaux'];
+    let selectionRobot = choix[(Math.floor(Math.random() * choix.length))];
+
+    if (selectionJoueur === selectionRobot) {
+        result = "égalite";
+    } else if (
+        (selectionJoueur === "pierre" && selectionRobot === "ciseaux") ||
+        (selectionJoueur === "feuille" && selectionRobot === "pierre") ||
+        (selectionJoueur === "ciseaux" && selectionRobot === "feuille")
+    ) {
+        result = "gagné !";
+    } else {
+        result = "perdu :(";
+    }
+
+    document.querySelector('#result').innerHTML = `
+     Tu as choisis ${selectionJoueur}<br>
+     Le robot a choisi ${selectionRobot}<br>
+     Donc tu as ${result}
+     `
+    return result
 }
