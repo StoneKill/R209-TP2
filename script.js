2b79d6540ba0b2e80bef9a795c9a7467828a88f2
StoneKill <yanis290105@outlook.fr>
2024-05-06 14:34:09 +0200

    Modif de fichiers, avancé du site

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnP = document.getElementById('P').addEventListener("click", PFC("P"))
+let btnF = document.getElementById('F').addEventListener("click", PFC("F"))
+let brnS = document.getElementById('C').addEventListener("click", PFC("C"))
+
+function PFC() {
+    
+}
